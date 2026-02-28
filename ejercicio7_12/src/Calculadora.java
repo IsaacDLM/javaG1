7bb7a50c93d1eadc7349f10c42a90e93fbaf4b12
IsaacDLM <isaac.lm.dev@gmail.com>
2026-02-28 15:40:13 +0100

    Ejercicio Calculadora de Ecuaciones de 2do Grado

--- ejercicio7_12/src/Calculadora.java
+++ ejercicio7_12/src/Calculadora.java
@@ -1,6 +1,44 @@
 public class Calculadora {
+    private double a;
+    private double b;
+    private double c;
 
-    public void solucion(){
+    public Calculadora(double a, double b, double c){
+        setA(a);
+        this.b = b;
+        this.c = c;
+    }
 
+    public void setA(double a){
+        if (a == 0){
+            System.out.println("'a' no puede ser 0");
+            this.a = 1;
+        }else {
+            this.a = a;
+        }
+    }
+    public void setB(double b){this.b = b;}
+    public void setC(double c){this.c = c;}
+
+    public double getDiscriminante(){
+        return Math.pow(b, 2) - (4 * a * c);
+    }
+
+    public boolean isDiscrimantePositivo(){
+        return getDiscriminante() > 0;
+    }
+    public double[] solucion(){
+         double[] soluciones;
+         double disc = getDiscriminante();
+
+         if(disc < 0){
+             System.out.println("La ecuacion no tiene soluciones reales");
+             soluciones = new double[0];
+         }else {
+             double x1 = (-b + Math.sqrt(disc)) / (2 * a);
+             double x2 = (-b - Math.sqrt(disc)) / (2 * a);
+             soluciones = new double[]{x1, x2};
+         }
+         return soluciones;
     }
 }
--- ejercicio7_12/src/Main.java
+++ ejercicio7_12/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Implementa una clase que permita resolver ecuacuiones de segundo grado. Los coeficientes
 pueden indicarse en el constuctor y modificarse a posteriori. Es fundamental que la clase disponga de
@@ -6,6 +8,32 @@ positivo.
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el coeficiente a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Introduce el coeficiente b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Introduce el coeficiente c: ");
+        double c = sc.nextDouble();
+
+        Calculadora nuevoCalculo = new Calculadora(a,b,c);
+
+        System.out.println("\n--- Resultados ---");
+        System.out.println("¿El discriminante es positivo? " + (nuevoCalculo.isDiscrimantePositivo() ? "Sí" : "No"));
+        System.out.println("Valor del discriminante: " + nuevoCalculo.getDiscriminante());
+
+        double[] soluciones = nuevoCalculo.solucion();
+
+        if (soluciones.length == 0) {
+            System.out.println("No existen soluciones en el conjunto de los números reales.");
+        } else if (soluciones.length == 1) {
+            System.out.println("La solución única es: " + soluciones[0]);
+        } else {
+            System.out.println("Solución 1 (x1): " + soluciones[0]);
+            System.out.println("Solución 2 (x2): " + soluciones[1]);
+        }
 
     }
 }
