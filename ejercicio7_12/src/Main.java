dafd991a93fe4d4c3d06765b2e3603f18f04a082
IsaacDLM <isaac.lm.dev@gmail.com>
2026-02-28 11:11:35 +0100

    Agrego gitignore para proyecto java

---
+++ ejercicio7_12/src/Main.java
@@ -0,0 +1,11 @@
+/*
+Implementa una clase que permita resolver ecuacuiones de segundo grado. Los coeficientes
+pueden indicarse en el constuctor y modificarse a posteriori. Es fundamental que la clase disponga de
+un metodo que devuelva las distintas soluciones y de un metodo que nos informe si el discrimiente es
+positivo.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
