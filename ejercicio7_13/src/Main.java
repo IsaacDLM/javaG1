9412a129f1356c7c42ba8b4fa4c0750567cb4b9a
IsaacLezametaMinaya <lezametadavid@gmail.com>
2026-03-01 21:47:27 +0100

    Creacion de clase Colores pc Ub

--- ejercicio7_13/src/Main.java
+++ ejercicio7_13/src/Main.java
@@ -1,4 +1,12 @@
+/*
+En el momento de decorar una casa, una habitacion o cualquier objeto, se plantea el problema de elegir la paleta
+de colores que vamos a utilizar en nuestra decoracion. Existe una solucion, algo atrevida, que consiste en utilizar colores
+al azar.
 
+Diseña la clase "COLORES", que alberga por defecto una serie de colores (mediante una cadena),
+aunque es posible añadir tantos como necesitesmo. La clase tendra un metodo que devuelve una tabla con los n colores
+que necesitemos elegidos al azar sin repeticiones.
+ */
 public class Main {
     public static void main(String[] args) {
 
